59a628ca3d39a67953b8fb2f74845d5ada0ddd5f
Nq force <79386552+conquestist@users.noreply.github.com>
2025-12-12 19:37:45 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+payload = {
+"text" :"nigga",
+"chat_id": "-5055440298"
+}
+
+
+req = requests.post("https://api.telegram.org/bot8407894551:AAFo2ITXhTlwE7gjFx8mKO36G5floRC2JRI/sendMessage", json=payload)
 
